1058d8e0ac7ba45e4cfa3634f7341cc405907d9d
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-05-03 23:28:53 +0900

    ディレクトリを変更

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(install_requires=[], entry_points={"console_scripts": ["ogimgen = ogimgen:generate"]})
+setup(install_requires=[], entry_points={"console_scripts": ["ogimgen = ogimgen.ogimgen:generate"]})
