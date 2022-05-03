2ce07f5dd4f1be11daeaa6cd31a2de10c1747bea
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-05-03 22:33:28 +0900

    command test

---
+++ ogimgen.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def generate():
+    print(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    generate()
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(install_requires=[], entry_points={"console_scripts": ["ogimgen = ogimgen:generate"]})
