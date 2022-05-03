c1d26b142be2f15b0623dce5d1935be6305ffd80
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-05-04 00:06:36 +0900

    markdownからyamlの部分を読み込む

--- ogimgen/ogimgen.py
+++ ogimgen/ogimgen.py
@@ -1,8 +1,29 @@
 import sys
+import os
+import yaml
 
+def get_markdown_yaml_metadata(markdown_path: str) -> str:
+    yaml_metadata: str = ""
+    is_reading_yaml_metadata: bool = False
+    with open(markdown_path, "r") as f:
+        for line in f:
+            if line == "---\n":
+                if not is_reading_yaml_metadata:
+                    is_reading_yaml_metadata = True
+                    continue
+                else:
+                    return yaml_metadata
+
+            if is_reading_yaml_metadata:
+                yaml_metadata += line
+
+    return yaml_metadata
 
 def generate():
-    print(sys.argv[1:])
+    yaml_metadata = get_markdown_yaml_metadata(os.path.join(os.getcwd(), sys.argv[1]))
+
+    data = yaml.load(yaml_metadata, Loader=yaml.SafeLoader)
+    print(data)
 
 
 if __name__ == "__main__":
