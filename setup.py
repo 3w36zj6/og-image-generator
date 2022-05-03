ff90be1b6794819707d5790828afa11fb99e9a8b
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-05-04 00:05:34 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup(install_requires=[], entry_points={"console_scripts": ["ogimgen = ogimgen.ogimgen:generate"]})
+setup(
+    name="ogimgen",
+    version="1.0.0",
+    install_requires=[],
+    entry_points={"console_scripts": ["ogimgen = ogimgen.ogimgen:generate"]},
+)
